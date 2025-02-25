951d199ec13abc1666cfcf932a3e1a012326fc7a
Robles54 <robleschristopher54@gmail.com>
2025-02-25 14:31:02 -0500

    Initial Commit in DemoProject

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Christopher Robles on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
